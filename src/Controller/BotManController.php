c8bfb9c49b467d6dd1525df27b1555a288bc9c4e
Sajid Salhi <sajid.s35400@gmail.com>
2024-01-19 08:52:11 +0100

    la partie du projet mis en public

--- src/Controller/BotManController.php
+++ src/Controller/BotManController.php
@@ -4,38 +4,56 @@ namespace App\Controller;
 
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
+use BotMan\Drivers\Web\WebDriver;
 use BotMan\BotMan\Drivers\DriverManager;
+use App\Controller\OnboardingConversation;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use BotMan\BotMan\Messages\Conversations\Conversation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class BotManController extends AbstractController{
 
     function messageAction(Request $request): Response
     {
-        DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
-
-        // Configuration du BotMan WebDriver
-        $config = [];
-
+        DriverManager::loadDriver(WebDriver::class);
+        $config = [
+            'web' => [
+                'matchingData' => [
+                    'driver' => 'web',
+                ],
+            ]
+        ];
         // Créer une instance BotMan
         $botman = BotManFactory::create($config);
-
+        
         // Donnez au bot des éléments à écouter.
-        $botman->hears('(salut||hello)', function (BotMan $bot) {
-            $bot->reply('salut!||hello');
+        $botman->hears('(salut)', function (BotMan $bot) {
+      #      $bot->startConversation(new OnboardingConversation);
+            $bot->reply('salut!');
+            $bot->ask('Quel est ton nom?', function($answer, $bot) {
+                $bot->say('bienvenue '.$answer->getText());
+            }); 
         });
 
         // Définir une solution secondaire
         $botman->fallback(function (BotMan $bot) {
-            $bot->reply('Désolée, je n ai pas compris.||Sorry, I did not understand');
+            $bot->reply('Désolée, je n ai pas compris.');
         });
 
         // Commencer à écouter/écrire
-        $botman->listen();
-
+    /*    $botman->listen();
+        $botman = BotManFactory::create($config);
+       
+        $conversation=new OnBoardingConversation;
+        $conversation->setBot($botman);
+        $conversation->startConversation($conversation);
+    
+    $data=$conversation->getDataFromChatBot();
+    return $this->render('chat.html.twig', ["data"=>$data]);
+  
+}*/
         return new Response();
     }
    
